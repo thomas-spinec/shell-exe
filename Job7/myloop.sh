6f6ecdba60f08f4c9c557d786dfd34e2bad4d8b7
thomas-spinec <thomas.spinec@laplateforme.io>
2022-09-28 16:01:56 +0200

    Job 7 amélioration

--- Job7/myloop.sh
+++ Job7/myloop.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #Une boucle qui s'incrémente jusqu'à 10
-var=1
 
-#
-while [ $var != 11 ]
+for i in {1..10}
 do 
-	echo "Je suis un script qui arrive à faire une boucle "$var
-	((var=var+1))
+	echo "Je suis un script qui arrive à faire une boucle "$i
 done
 
