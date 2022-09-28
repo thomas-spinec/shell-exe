83e44eaedd95f13b85fa562d8910aa599b4d5524
thomas-spinec <thomas.spinec@laplateforme.io>
2022-09-28 14:07:34 +0200

    Job 6 done

---
+++ Job6/my_calculator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Script pour créer une calculatrice avec des conditions
+
+if [ $2 = "+" ]
+then
+	result=$(($1 + $3))
+	echo "Vous avez choisi l'addition"
+elif [  $2 = "-" ]
+then
+	result=$(($1 - $3))
+	echo "Vous avez choisi la soustraction"
+elif [ $2 = "x" ]
+then
+	result=$(($1 * $3))
+	echo "Vous avez choisi la multiplication"
+elif [ $2 = "/" ]
+then
+	result=$(($1 / $3))
+	echo "Vous avez choisi la division"
+else
+	echo "ce n'est pas un bon opérateur, essayer avec +, -, x ou /"
+fi
+
+echo "Result : "$result
